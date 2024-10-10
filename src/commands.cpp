e57a4a40e1223789aa2297f65d216e4dfed604e1
MaxChip <1Max0Lag1@gmail.com>
2024-10-09 21:22:46 -0400

    added keys and coloured tect checks

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,17 +1,30 @@
 #include <ncurses.h>
 #include "commands.h"
-#include <unordered_map>
+#include <signal.h>
 
+bool ctrl;
+bool shift;
+bool fn;
+bool cmd;
+bool capslock;
+bool alt;
 
 namespace commands
 {
+
     Command_Return prompt_command()
     {
-        
+        Command_Return cr;
+        cr.cmd_name = "null";
+        cr.content = "null";
+        return cr;
     }
 
     Command_Return macro_command()
     {
-
+        Command_Return cr;
+        cr.cmd_name = "null";
+        cr.content = "null";
+        return cr;
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,101 @@
 #include <ncurses.h>
 #include <rapidjson/document.h>
 #include <iostream>
+#include <unistd.h>
+#include <signal.h>
 
-bool on = true;
+// $ Salsa file.txt
+// $ Salsa --config
+// $ Salsa -c 
+
+void force_stop(int signal) {}
 
 int main (int argc, char *argv[])
 {
-    int line = 0;
-    char ch;
+
+    signal(SIGINT, force_stop); // block ctrl + c force end
+    signal(SIGTSTP, force_stop); // block ctrl + z force end
+
+    int ch;
+    bool on = true;
+
     initscr();
     cbreak();
     noecho();
-    //mvaddch(5, 2, 'f');
+    keypad(stdscr, TRUE);
+    curs_set(0);
     refresh();
-    while (on)
-    {
-        ch = getch();
-        addch((ch));
-        line++;
-        if (ch == 27)
+    if (has_colors()) {
+        while (on)
         {
-            on = false;
+            ch = getch();
+            
+            // escape keys
+            if (ch == 27) {
+                printw("(escape)");
+                on = false;
+            } else if (ch == KEY_UP) {
+                printw("(up)");
+            } else if (ch == KEY_DOWN) {
+                printw("(down)");
+            } else if (ch == KEY_LEFT) {
+                printw("(left)");
+            } else if (ch == KEY_RIGHT) {
+                printw("(right)");
+            } else if (ch == KEY_BACKSPACE) {
+                printw("(backspace)");
+            } else if (ch == KEY_HOME) {
+                printw("(home)");
+            } else if (ch == KEY_END) {
+                printw("(end)");
+            } else if (ch == KEY_PPAGE) {
+                printw("(page up)");
+            } else if (ch == KEY_NPAGE) {
+                printw("(page down)");
+            } else if (ch == 265) {
+                printw("(f1)");
+            } else if (ch == 266) {
+                printw("(f2)");
+            } else if (ch == 267) {
+                printw("(f3)");
+            } else if (ch == 268) {
+                printw("(f4)");
+            } else if (ch == 269) {
+                printw("(f5)");
+            } else if (ch == 270) {
+                printw("(f6)");
+            } else if (ch == 271) {
+                printw("(f7)");
+            } else if (ch == 272) {
+                printw("(f8)");
+            } else if (ch == 273) {
+                printw("(f9)");
+            } else if (ch == 274) {
+                printw("(f10)");
+            } else if (ch == KEY_DC) {
+                printw("(del)");
+            } else if (ch == KEY_IC) {
+                printw("(insert)");
+            } else if (ch == KEY_SF) {
+                printw("(scroll down)");
+            } else if (ch == KEY_SR) {
+                printw("(scroll up)");
+            } else if (ch == KEY_A1) {
+                printw("(home 1)");
+            } else if (ch == KEY_A3) {
+                printw("(home 2)");
+            } else if (ch == KEY_C1) {
+                printw("(end 1)");
+            } else if (ch == KEY_C3) {
+                printw("(end 2)");
+            } else {
+                addch(char(ch));
+            }
         }
-    }
     endwin();
+    } else {
+        std::cerr << "Your terminal does not support coloured text" << std::endl;
+        return 1;
+    }
 	return 0;
 }
